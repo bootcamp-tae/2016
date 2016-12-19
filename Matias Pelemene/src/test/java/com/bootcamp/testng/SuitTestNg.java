bebf58b6112ce8dda3abaff96850a0f7380b09a8
Matias Pelemene <correochuky@gmail.com>
2016-12-19 20:38:45 -0300

    Primeros Pasos

---
+++ Matias Pelemene/src/test/java/com/bootcamp/testng/SuitTestNg.java
@@ -0,0 +1,14 @@
+package com.bootcamp.testng;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Colegio on 19/12/2016.
+ */
+public class SuitTestNg {
+
+    @Test
+    public void test1(){
+
+    }
+}
