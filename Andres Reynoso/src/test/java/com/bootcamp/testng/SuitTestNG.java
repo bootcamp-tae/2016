8c3e0558e92856c5f9212844283ba944ef51e4c8
AndresReynoso <andresreynoso12@gmail.com>
2016-12-19 20:38:58 -0300

    BootCampAndres

---
+++ Andres Reynoso/src/test/java/com/bootcamp/testng/SuitTestNG.java
@@ -0,0 +1,13 @@
+package com.bootcamp.testng;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Colegio on 19/12/2016.
+ */
+public class SuitTestNG {
+    @Test
+    public void test1() {
+
+    }
+}
