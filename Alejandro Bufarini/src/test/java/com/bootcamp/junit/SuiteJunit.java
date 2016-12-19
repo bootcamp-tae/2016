69cfd3276c1a9a81d246d990d86ccd1263723c31
Alejandro <alejandroebufarini@gmail.com>
2016-12-19 20:38:47 -0300

    Alejandro1

---
+++ Alejandro Bufarini/src/test/java/com/bootcamp/junit/SuiteJunit.java
@@ -0,0 +1,14 @@
+package com.bootcamp.junit;
+
+import org.junit.Test;
+
+/**
+ * Created by Colegio on 19/12/2016.
+ */
+public class SuiteJunit {
+
+    @Test
+    public void test1(){
+
+    }
+}
