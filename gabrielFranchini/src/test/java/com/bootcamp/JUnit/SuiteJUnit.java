6b5da45765ea05e7d3671eefc0a9da34b10c2f07
Gabriel <gabo.mdq.96@gmail.com>
2016-12-21 18:38:29 -0300

    Add first commit

---
+++ gabrielFranchini/src/test/java/com/bootcamp/JUnit/SuiteJUnit.java
@@ -0,0 +1,13 @@
+package com.bootcamp.JUnit;
+import org.junit.Test;
+
+/**
+ * Created by Colegio on 19/12/2016.
+ */
+public class SuiteJUnit {
+
+    @Test
+    public void test1(){
+
+    }
+}
---
+++ gabrielFranchini/src/test/java/com/bootcamp/testNG/SuiteTestNG.java
@@ -0,0 +1,13 @@
+package com.bootcamp.testNG;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Colegio on 19/12/2016.
+ */
+public class SuiteTestNG {
+
+    @Test
+    public void test1(){
+
+    }
+}
