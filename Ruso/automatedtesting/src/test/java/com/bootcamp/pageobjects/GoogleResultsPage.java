9342e79ec467348e237be0d22b70b8ceac57af1b
Juan Krzemien <juan.krzemien@globant.com>
2016-12-21 23:51:26 -0300

    Day 2 - Test Runners. General WebDriver usage. Page Objects overview

--- Ruso/automatedtesting/src/test/java/com/bootcamp/pageobjects/GoogleResultsPage.java
+++ Ruso/automatedtesting/src/test/java/com/bootcamp/pageobjects/GoogleResultsPage.java
@@ -1,4 +1,4 @@
-package com.bootcamp.junit.web.pages;
+package com.bootcamp.pageobjects;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
--- Ruso/automatedtesting/src/test/java/com/bootcamp/testng/SuiteTestNG.java
+++ Ruso/automatedtesting/src/test/java/com/bootcamp/testng/SuiteTestNG.java
@@ -1,57 +1,39 @@
-package com.bootcamp.junit;
+package com.bootcamp.testng;
 
 import com.bootcamp.pageobjects.GoogleHomePage;
 import com.bootcamp.pageobjects.GoogleResultsPage;
 import io.github.bonigarcia.wdm.Architecture;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import io.github.bonigarcia.wdm.InternetExplorerDriverManager;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-
-import java.util.Arrays;
+import org.testng.annotations.*;
 
 /**
  * Day 2 - General WebDriver usage
  * <p>
  * Overview of Page Objects
- * JUnit parameterized tests
+ * TestNG parameterized tests
  *
  * @author Juan Krzemien
  */
 
-@RunWith(Parameterized.class)
-public class SuiteJUnit {
+public class SuiteTestNG {
 
-    private WebDriver browser;
+    private final WebDriver browser;
 
-    public SuiteJUnit(WebDriver browser) {
-        this.browser = browser;
+    public SuiteTestNG(WebDriver driver) {
+        this.browser = driver;
     }
 
-    @Parameters(name = "{index}:Browser[{0}]")
-    public static Iterable<Object[]> data() {
-        InternetExplorerDriverManager.getInstance().setup(Architecture.x32);
-        ChromeDriverManager.getInstance().setup();
-        return Arrays.asList(new Object[][]{
-                {new InternetExplorerDriver()},
-                {new ChromeDriver()}}
-        );
-    }
-
-    @Before
+    @BeforeMethod(alwaysRun = true)
     public void setUp() {
         browser.manage().window().maximize();
         browser.navigate().to("http://www.google.com");
     }
 
-    @After
+    @AfterMethod(alwaysRun = true)
     public void tearDown() {
         browser.quit();
     }
@@ -61,7 +43,24 @@ public class SuiteJUnit {
         GoogleHomePage home = new GoogleHomePage(browser);
         GoogleResultsPage results = home.doSearch("Something");
         results.clickOnResultNumber(1);
-        Thread.sleep(3000);
+    }
+
+    public static class TestFactory {
+
+        @DataProvider(name = "browsers")
+        public static Object[][] data() {
+            InternetExplorerDriverManager.getInstance().setup(Architecture.x32);
+            ChromeDriverManager.getInstance().setup();
+            return new Object[][]{
+                    {new InternetExplorerDriver()},
+                    {new ChromeDriver()}
+            };
+        }
+
+        @Factory(dataProvider = "browsers")
+        public Object[] createTestInstances(WebDriver driver) {
+            return new Object[]{new SuiteTestNG(driver)};
+        }
     }
 
 }
